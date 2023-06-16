a4ce5af864156539730bc423e1008168a13a4216
faizanulhassan32 <faizanulhassan043@gmail.com>
2023-06-16 19:54:29 +0500

    final commit

--- src/components/ImgsOverlappedComp.js
+++ src/components/ImgsOverlappedComp.js
@@ -7,9 +7,9 @@ export default function ImgsOverlapped() {
             <div className="container">
                 <h2 className="heading">Contract Intelligence to drive financial <br /> performance</h2>
                 <div className="image-container">
-                    <img className="image-back" src="pic.png" alt="left back image here" />
-                    <img className="image-front" src="pic.png" alt="front centre image here" />
-                    <img className="image-back" src="pic.png" alt="right back image here" />
+                    <img className="image-back" src="pic.png" alt="Stats on left side under" />
+                    <img className="image-front" src="pic.png" alt="Stats on main side " />
+                    <img className="image-back" src="pic.png" alt="Stats on right side under" />
                 </div>
                 <p className="description">Terzo helps leaders <span className="highlight">track every dollar</span> coming in <br /> and going out of the organization</p>
                 <button className="bookdemo-button1" style={{ color: "black", fontWeight: "bold" }}>Learn More</button>
